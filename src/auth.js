4dde8c385d5ce24b26bac3c0f190f5a48b9de50c
fullstack <fullstack@test.com>
2020-02-04 08:28:25 +0100

    Have done auth.js, product.js, manifest.js Refactoring

--- src/auth.js
+++ src/auth.js
@@ -1,90 +1,65 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
 import Header from './elements/header';
 import InputMask from "react-input-mask";
 
 export default class Auth extends Component {
+    state = {
+        submitted: false
+    }
 
-  state = {
-    submitted: false
-  }
+    render(){
+        return (
+            <div>
+                <Header></Header>
+                <div className="row">
+                    <div className="col s12 m12 m6">
+                        <div className="card darken-1">
+                            <div className="card-content">
+                                <div>
+                                    <form>
+                                        {!this.state.submitted && <div className="row">
+                                            <div className={'col s12 m12'}>
+                                                <ul className="collection">
+                                                    <li className="collection-item inverted">
+                                                        <div className="heading">Enter Phone Number</div>
+                                                    </li>
+                                                    <li className="collection-item">
+                                                        <div className="input-field">
+                                                            <InputMask mask="(999) - 999 - 9999" alwaysShowMask={true} className="font"/>
+                                                        </div>
+                                                        <div className="input-field">
+                                                            <a className="waves-effect waves-light btn" onClick={() => this.setState({submitted: true})}>Submit</a>
+                                                        </div>
+                                                    </li>
 
-  render(){
-    return (
-      <div>
-      <Header></Header>
-      <div className="row">
-  <div className="col s12 m12 m6">
-    <div className="card darken-1">
-      <div className="card-content">
-        
-        <div>
-        <form>
-          {!this.state.submitted && <div className="row">
-      <div className={'col s12 m12'}>
-      <ul className="collection">
-                <li className="collection-item inverted">
-                <div className="heading">Enter Phone Number</div>
-                </li>
-                <li className="collection-item">
-                <div className="input-field">
-                <InputMask mask="(999) - 999 - 9999" alwaysShowMask={true} className="font"/>
-      </div>
-      <div className="input-field">
-      <a className="waves-effect waves-light btn" onClick={() => this.setState({submitted: true})}>Submit</a>
-      </div>
-                </li>
-                
-        </ul>
-      </div>
-</div>
-}
-    {this.state.submitted && <div className="row">
-      <div className={'col s12 m12'}>
-      <ul className="collection">
-                <li className="collection-item inverted">
-                <div className="heading">Enter 2FA code</div>
-                </li>
-                <li className="collection-item">
-                <div className="input-field">
-                <InputMask mask="999999" alwaysShowMask={true} className="font"/>
-      </div>
-      <div className="input-field">
-      <a className="waves-effect waves-light btn" onClick={() => this.setState({submitted: true})}>Submit</a>
-      </div>
-                </li>
-
-                {/* <li className="collection-item">
-                <div className="input-field">
-        <input id="last_name" type="text" className="validate"/>
-        <label htmlFor="last_name">Enter Code</label>
-      </div>
-      <div className="input-field">
-      <a className="waves-effect waves-light btn">Submit</a>
-      </div>
-        
-                </li>
-                 */}
-        </ul>
-      </div>
-</div>}
-
-    
-    </form>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
-</div>
-  )
-  }
-    
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        }
+                                        {this.state.submitted && <div className="row">
+                                            <div className={'col s12 m12'}>
+                                                <ul className="collection">
+                                                    <li className="collection-item inverted">
+                                                        <div className="heading">Enter 2FA code</div>
+                                                    </li>
+                                                    <li className="collection-item">
+                                                        <div className="input-field">
+                                                            <InputMask mask="999999" alwaysShowMask={true} className="font"/>
+                                                        </div>
+                                                        <div className="input-field">
+                                                            <a className="waves-effect waves-light btn" onClick={() => this.setState({submitted: true})}>Submit</a>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>}
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
