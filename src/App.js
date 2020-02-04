47fa5a0110091ff998d18e3c6c949d1a044e75ec
fullstack <fullstack@test.com>
2020-02-04 08:32:07 +0100

    Have done App.js Refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'materialize-css/dist/css/materialize.min.css'
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from "react-router-dom";
 import Regulars from './regulars';
 import RegularsAdd from './regulars_add';
@@ -19,47 +17,44 @@ import Manifest from './manifest';
 import ProductManifestReport from './product-manifest-reports';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-      
-      
-      <Switch>
-          <Route path="/regulars">
-            <Regulars />
-          </Route>
-          <Route path="/regulars-add">
-            <RegularsAdd />
-          </Route>
-          <Route path="/reports">
-            <Reports />
-          </Route>
-          <Route path="/products">
-            <Products/>
-          </Route>
-          <Route path="/display-name">
-            <DisplayName/>
-          </Route>
-          <Route path="/transactions">
-            <Transactions/>
-          </Route>
-          <Route path="/manifest">
-            <Manifest/>
-          </Route>
-          <Route path="/product-manifest-reports">
-            <ProductManifestReport/>
-          </Route>
-          <Route path="/auth">
-            <Auth/>
-          </Route>
-          <Route path="/">
-            <Regulars />
-          </Route>
-        </Switch>
-      
-    </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/regulars">
+                        <Regulars />
+                    </Route>
+                    <Route path="/regulars-add">
+                        <RegularsAdd />
+                    </Route>
+                    <Route path="/reports">
+                        <Reports />
+                    </Route>
+                    <Route path="/products">
+                        <Products/>
+                    </Route>
+                    <Route path="/display-name">
+                        <DisplayName/>
+                    </Route>
+                    <Route path="/transactions">
+                        <Transactions/>
+                    </Route>
+                    <Route path="/manifest">
+                        <Manifest/>
+                    </Route>
+                    <Route path="/product-manifest-reports">
+                        <ProductManifestReport/>
+                    </Route>
+                    <Route path="/auth">
+                        <Auth/>
+                    </Route>
+                    <Route path="/">
+                        <Regulars />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
