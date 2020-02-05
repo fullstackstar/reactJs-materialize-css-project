ec4e8201e295ee2cc7f6430f8ce3498900fc5bd6
fullstack <fullstack@test.com>
2020-02-05 06:22:30 +0100

    Have done regulars.js Refactoring

--- src/regulars.js
+++ src/regulars.js
@@ -1,42 +1,36 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 import Header from './elements/header';
 
 export default function Regulars() {
     return (
         <div>
-        <Header></Header>
-        <div className="row">
-    <div className="col s12 m12 m6">
-      <div className="card darken-1">
-        <div className="card-content">
-          <span className="card-title">Regulars <Link className="float-right" to="/regulars-add"><i className="material-icons">add</i></Link></span>
-          <div>
-          <form>
-      <div className="row">
-        <div className={'col s12 m12'}>
-          <ul className="collection">
-            <li className="collection-item inverted">Display Name</li>
-            <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
-            <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
-            <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
-          </ul>
+            <Header></Header>
+            <div className="row">
+                <div className="col s12 m12 m6">
+                    <div className="card darken-1">
+                        <div className="card-content">
+                            <span className="card-title">Regulars <Link className="float-right" to="/regulars-add"><i className="material-icons">add</i></Link></span>
+                            <div>
+                                <form>
+                                    <div className="row">
+                                        <div className={'col s12 m12'}>
+                                            <ul className="collection">
+                                                <li className="collection-item inverted">Display Name</li>
+                                                <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
+                                                <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
+                                                <li className="collection-item">Alvin<span className="float-right color-red"><i className="material-icons">delete</i></span></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    
-      </div>
-      </form>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-    
-  </div>
-  </div>
     )
 }
